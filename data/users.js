1fb8e8f5da6be08022144d81a3d27f416a82275a
Ahmed Hammad <hammad97official@gmail.com>
2020-04-06 00:47:34 +0200

    Update mocked data

--- data/users.js
+++ data/users.js
@@ -3,36 +3,36 @@ module.exports = [
         id: 0,
         name: "Ibn-Rushd",
         age: 25,
-        country: "England"
+        country: {name: "England"}
     },
     {
         id: 1,
         name: "Thomas Aquinas",
         age: 28,
-        country: "France"
+        country: {name: "France"}
     },
     {
         id: 2,
         name: "Al-Kindi",
         age: 37,
-        country: "Egypt"
+        country: {name: "Egypt"}
     },
     {
         id: 3,
         name: "René Descartes",
         age: 12,
-        country: "Canada"
+        country: {name: "Canada"}
     },
     {
         id: 4,
         name: "Ibn-Sina",
         age: 41,
-        country: "China"
+        country: {name: "China"}
     },
     {
         id: 5,
         name: "Aristotle",
         age: 34,
-        country: "Brazil"
+        country: {name: "Brazil"}
     }
 ]
